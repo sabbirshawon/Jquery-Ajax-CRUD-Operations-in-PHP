5788d8537ee0084fe22bb9424d9e6fb82d94c245
Sabbir Ahmed <sabbirs93@gmail.com>
2019-01-09 17:17:01 +0600

    Create db_config.php

---
+++ api/db_config.php
@@ -0,0 +1,10 @@
+<?php
+  
+    $hostName = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "db_test";
+ 
+    $mysqli = new mysqli($hostName, $username, $password, $dbname);
+ 
+?>
